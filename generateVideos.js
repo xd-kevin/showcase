5d84a8523100aa8706d97cd9c25ef991891675d9
ynsun <sunyanan@odc.cmbchina.cn>
2019-01-11 15:22:22 +0800

    add automatic reader form files

---
+++ generateVideos.js
@@ -0,0 +1,51 @@
+const weishiFolder = "./src/assets/weishi/";
+const gifFolder = "./src/assets/gif/";
+const thumbFolder = "./src/assets/thumb/";
+const fs = require("fs");
+const videoNamesConfig = {};
+let videoNames = [];
+
+function getVideoNames(folderName) {
+  return new Promise((resolve, reject) => {
+    const names = [];
+    fs.readdir(folderName, (err, files) => {
+      if (err) {
+        reject(err);
+        return;
+      }
+      files.forEach(file => {
+        names.push(file);
+      });
+      resolve(names);
+    });
+  });
+}
+
+getVideoNames(weishiFolder)
+  .then(names => {
+    videoNames = names.map(name => name.slice(0, -4));
+    videoNames.forEach(name => {
+      videoNamesConfig[name] = {
+        video: [name + ".mp4"]
+      };
+    });
+    return getVideoNames(gifFolder);
+  })
+  .then(gifNames => {
+    videoNames.forEach(name => {
+      videoNamesConfig[name].gif = gifNames.filter(gifName =>
+        gifName.includes(name)
+      );
+    });
+    return getVideoNames(thumbFolder);
+  })
+  .then(thumbNames => {
+    videoNames.forEach(name => {
+      videoNamesConfig[name].thumb = thumbNames.filter(thumbName =>
+        thumbName.includes(name)
+      );
+    });
+
+    const json = JSON.stringify(videoNamesConfig);
+    fs.writeFile("./src/assets/videoNames.json", json, "utf8");
+  });
