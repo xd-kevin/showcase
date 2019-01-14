8294cc29536ea121f4f77186f65339c357ebd713
ynsun <sunyanan@odc.cmbchina.cn>
2019-01-14 10:25:21 +0800

    add next & before link

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,36 +1,52 @@
-import { Component, ElementRef } from "@angular/core";
-import { Observable, Subject } from "rxjs";
-import { HttpClient } from "@angular/common/http";
+import { Component, ElementRef } from '@angular/core';
+import { Observable, Subject } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
-  selector: "app-root",
-  templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.scss"]
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   videoNames = [];
   config = null;
-  selectedName = this.videoNames[0];
+  selectedNameIndex = 0;
+  selectedName = '';
   isSelectedVideoImageWidth$;
 
   constructor(private elmRef: ElementRef, private http: HttpClient) {
     this.isSelectedVideoImageWidth$ = new Subject();
     this.getVideoNames().subscribe(data => {
       this.videoNames = Object.keys(data);
+      this.selectedNameIndex = 0;
+      this.selectedName = this.videoNames[this.selectedNameIndex];
       this.config = data;
     });
   }
 
-  handleVideoChange() {
-    const player = this.elmRef.nativeElement.querySelector("video");
-    if (player) player.load();
+  updateVideo() {
+    this.selectedName = this.videoNames[this.selectedNameIndex];
+    const player = this.elmRef.nativeElement.querySelector('video');
+    if (player) {
+      player.load();
+    }
     this.getImageDimension(
-      this.config[this.selectedName].gif[0] ||
-        this.config[this.selectedName].thumb[0]
+      this.config[this.selectedName].gif[0] || this.config[this.selectedName].thumb[0]
     );
   }
 
-  getImageDimension(src) {
+  handleVideoChange(id) {
+    if (id >= 0 && id < this.videoNames.length) {
+      this.selectedNameIndex = id;
+      this.updateVideo();
+    }
+  }
+
+  parseInt(id) {
+    return parseInt(id, 10);
+  }
+
+  private getImageDimension(src) {
     const img = new Image();
     const isSelectedVideoImageWidth$ = this.isSelectedVideoImageWidth$;
     img.onload = function(event) {
@@ -40,7 +56,7 @@ export class AppComponent {
     img.src = `./assets/gif/${src}`;
   }
 
-  public getVideoNames(): Observable<any> {
-    return this.http.get("./assets/videoNames.json");
+  private getVideoNames(): Observable<any> {
+    return this.http.get('./assets/videoNames.json');
   }
 }
